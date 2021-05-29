b9c76b6e0cd1666539db3bb5a74c2fa9a4b84470
Juanjo Jaramillo <juan.jaramillo02@epn.edu.ec>
2021-05-28 22:08:49 -0500

    new design

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
-import Image from 'next/image'
+import ComputerIcon from '../components/ComputerIcon'
 import styles from '../styles/Home.module.css'
+import {AiFillFacebook, AiFillInstagram, AiOutlineTwitter} from 'react-icons/ai'
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
         <meta name="robots" content="follow, index" />
         <meta content="Aplica aquí" name="description" />
         <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.club-software-epn.tech/" />
         <meta property="og:site_name" content="Club de software EPN"/>
         <meta property="og:description" content="Se parte de nuestra comunidad"/>
         <meta property="og:title" content="Club de software EPN" />
@@ -24,27 +26,40 @@ export default function Home() {
         <meta name="twitter:title" content="Club de software EPN" />
         <meta name="twitter:description" content="Se parte de nuestra comunidad" />
         <meta name="twitter:image" content="https://www.epn.edu.ec/wp-content/uploads/2010/09/DSC00179.jpg" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Mulish&family=Prompt:wght@400;600&display=swap" rel="stylesheet" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
           Club de Software EPN
         </h1>
-
-        <p className={styles.description}>
-          Bienvenido
-        </p>
-
+        <h2>Sitio web en construcción</h2>
+        <ComputerIcon height='25%'  className={styles.image}/>
         <div className={styles.grid}>
-          <a href="https://forms.office.com/r/eULae9Pe5z" className={styles.card}>
+          <a target="_blank" href="https://forms.office.com/r/eULae9Pe5z" className={styles.card}>
             <h2>Sé parte de nuestra comunidad</h2>
             <p>Aplica aquí</p>
           </a>
         </div>
       </main>
-
       <footer className={styles.footer}>
-      From<span className={styles.flag}>🇪🇨</span>with love 💚
+      <div className={styles.socialContainer}>
+        <a href='https://www.facebook.com/club.software.epn' target='_blank'>
+          <AiFillFacebook className={styles.socialIcon} fontSize='30px'/>
+        </a>
+
+        <a href='https://www.instagram.com/clubsoftwareepn/' target='_blank'>
+          <AiFillInstagram  className={styles.socialIcon} fontSize='30px'/>
+        </a>
+
+        <a href='https://twitter.com/clubSoftwareEpn' target='_blank'>
+          <AiOutlineTwitter className={styles.socialIcon} fontSize='30px'/>
+        </a>
+      </div>
+      <div>
+        De<span className={styles.flag}>🇪🇨</span>   con amor 💚  para todo el mundo
+      </div>
       </footer>
     </div>
   )
